b497085e8d472e34b64c08c9606f6fdcbad37da9
ichami630 <brandonichami@gmail.com>
2026-03-22 21:23:06 +0100

    faiss ivfflat

--- face-recognition-service/app/services/attendance_service.py
+++ face-recognition-service/app/services/attendance_service.py
@@ -12,8 +12,13 @@ faiss_index = None
 
 
 def load_users_into_memory():
+    """
+    Load all stored user face embeddings from database 
+    normalize them
+    put them into FAISS index for fast searching
+    """
     global user_ids, faiss_index
-    db = SessionLocal()
+    db = SessionLocal()  # connect to db
     try:
         face_templates = db.query(BiometricProfile).all()
 
@@ -27,6 +32,7 @@ def load_users_into_memory():
                 user_ids.append(s.user_id)
 
         if embeddings_list:
+            # converts list into a matrix(stacks each embeddings as rows in a new 2D array)
             embeddings = np.stack(embeddings_list).astype("float32")
 
             # normalize for cosine similary
@@ -35,24 +41,45 @@ def load_users_into_memory():
             # reshape to 512
             dimension = embeddings.shape[1]
 
-            # use inner product (cosine similarity)
-            faiss_index = faiss.IndexFlatIP(dimension)
+            quantifier = faiss.IndexFlatIP(dimension)  # creates quantifier
 
-            faiss_index.add(embeddings)
+            # create IVF index
+            nlist = max(1, min(50, len(embeddings_list)))  # number of clusters
+
+            # create IVF index
+            faiss_index = faiss.IndexIVFFlat(
+                quantifier,
+                dimension,
+                nlist,
+                faiss.METRIC_INNER_PRODUCT
+            )
+
+            # train the index
+            faiss_index.train(embeddings)
+
+            faiss_index.add(embeddings)  # store all users in FAISS
     finally:
         db.close()
 
 
 def find_best_match(new_embedding: np.ndarray):
+    """
+    Take a new face embedding 
+    compare it with stored users 
+    return the closest match
+    """
     global faiss_index, user_ids
 
     if faiss_index is None or faiss_index.ntotal == 0:
         return None, 0.0
 
-    # reshape and normalize
+    # reshape new embeddings from [512] to [[512]] since FAISS expects a 2D array
     query = new_embedding.reshape(1, -1).astype("float32")
+
+    # normalize (make the vector length 1)
     faiss.normalize_L2(query)
 
+    # compare query with all stored embeddings and returns the best match only
     distances, indices = faiss_index.search(query, k=1)
 
     best_idx = indices[0][0]
