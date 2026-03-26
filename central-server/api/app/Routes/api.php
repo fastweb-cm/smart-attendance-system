2a90db3f91b5eaff3ebe0dd0941a1cceb07f6514
ichami630 <brandonichami@gmail.com>
2026-03-26 20:45:56 +0100

    branch crud

--- central-server/api/app/Routes/api.php
+++ central-server/api/app/Routes/api.php
@@ -4,6 +4,7 @@ use App\Modules\Users\Controllers\UserRegistrationController;
 use App\Modules\Users\Controllers\UserController;
 use App\Modules\Users\Controllers\AuthController;
 use App\Middleware\AuthMiddleware;
+use App\Modules\Branch\Controllers\BranchController;
 
 /*
 |--------------------------
@@ -15,7 +16,6 @@ $router->get('/', [UserRegistrationController::class, 'index']);
 $router->post('/api/v1/auth/login', [AuthController::class, 'login']);
 $router->post('/api/v1/auth/logout', [AuthController::class, 'logout']);
 $router->post('/api/v1/auth/refresh', [AuthMiddleware::class, 'attempRefresh']);
-$router->post('/api/v1/auth', [AuthController::class, 'me']);
 
 
 /*
@@ -24,9 +24,16 @@ $router->post('/api/v1/auth', [AuthController::class, 'me']);
 |--------------------------
 */
 
+
 $router->group(['middleware' => [AuthMiddleware::class]], function($router) {
 
     $router->get('/api/v1/users', [UserController::class, 'index']);
     $router->post('/api/v1/users', [UserRegistrationController::class, 'register']);
 
+    //branch routes
+    $router->post('/api/v1/branch', [BranchController::class, 'store']);
+    $router->get('/api/v1/branch', [BranchController::class, 'all']);
+    $router->get('/api/v1/branch/{branchId}', [BranchController::class, 'one']);
+    $router->delete('/api/v1/branch/{id}', [BranchController::class, 'delete']);
+    $router->put('/api/v1/branch/{branchId}', [BranchController::class, 'edit']);
 });
